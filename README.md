725aaf5855a316d6d928e9b4b48ab4903257abaa
Kevin Huang <origamijr@sbcglobal.net>
2019-11-04 11:35:53 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MusicFun
+
+This is going to be the hub of midi/audio experiments I do in python (As soon as I have time).
---
+++ main.py
@@ -0,0 +1,9 @@
+import wx
+import mido
+
+app = wx.App()
+
+frame = wx.Frame(None, title='Title')
+frame.Show()
+
+app.MainLoop()
